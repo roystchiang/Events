da8911e49e9372826222db013f6fb197acc893c2
crosscent <crosscent@gmail.com>
2014-09-29 09:27:34 -0700

    fixed events/views.py

--- events/views.py
+++ events/views.py
@@ -10,7 +10,8 @@ def event(request):
     events = Events.objects.order_by('id')
     return render_to_response(
         'events/events.html',
-        {'events': events}
+        {'events': events},
+		context_instance=RequestContext(request)
     )
 def event_detail(request, event_id):
     event = get_object_or_404(Events, pk=event_id)
